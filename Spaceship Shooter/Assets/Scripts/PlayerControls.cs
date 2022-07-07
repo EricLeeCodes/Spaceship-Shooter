d119611ff619937d182d74cdb8b2050de758429b
Eric Lee <87954849+EricLeeCodes@users.noreply.github.com>
2022-07-06 23:00:08 -0500

    Added movement to ship
    Added independent movement to spaceship

---
+++ Spaceship Shooter/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    //New way ***
+    //[SerializeField] InputAction movement;
+
+    [SerializeField] float movementSpeed = 30f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 7f;
+
+    [SerializeField] float positionPitchFactor = 2f;
+    [SerializeField] float controlPitchFactor = -2f;
+    [SerializeField] float positionYawFactor = 3f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow;
+    float yThrow;
+
+    //New way ***
+    //private void OnEnable()
+    //{
+    //    movement.Enable();
+    //}
+
+    //private void OnDisable()
+    //{
+    //    movement.Disable();
+    //}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+    private void ProcessTranslation()
+    {
+        //New way ***
+        //xThrow = movement.ReadValue<Vector2>().x;
+        //yThrow = movement.ReadValue<Vector2>().y;
+
+
+        //Old way
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
+
+        //movement going right
+        float xOffset = xThrow * Time.deltaTime * movementSpeed;
+        float rawXPosition = transform.localPosition.x + xOffset;
+        float clampedXPosition = Mathf.Clamp(rawXPosition, -xRange, xRange);
+
+        float yOffset = yThrow * Time.deltaTime * movementSpeed;
+        float rawYPosition = transform.localPosition.y + yOffset;
+        float clampedYPosition = Mathf.Clamp(rawYPosition, -yRange, yRange);
+
+
+        transform.localPosition = new Vector3(
+            clampedXPosition,
+            clampedYPosition,
+            transform.localPosition.z);
+
+
+       
+    }
+
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+
+        float rollDueToControlThrow = xThrow * controlRollFactor;
+
+
+        float pitch = pitchDueToPosition * pitchDueToControlThrow;
+        float yaw = yawDueToPosition;
+        float roll = rollDueToControlThrow;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+}
