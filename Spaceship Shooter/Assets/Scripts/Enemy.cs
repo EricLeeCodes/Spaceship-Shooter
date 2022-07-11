ed817effcf7dea7a8feb52ec15af0611ec8160e2
Eric Lee <87954849+EricLeeCodes@users.noreply.github.com>
2022-07-11 02:17:12 -0500

    Added Enemy Explosions and Point systems
    Added Enemy Explosions and Point systems to the game

--- Spaceship Shooter/Assets/Scripts/Enemy.cs
+++ Spaceship Shooter/Assets/Scripts/Enemy.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+
+    private void Start()
+    {
+        //FindObjectOfType is ONLY resource intensive IF put into ///UPDATE///
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
     private void OnParticleCollision(GameObject other)
     {
-        Debug.Log($"{name} was hit by {other.gameObject.name}");
+        ProcessHit();
+        KillEnemy();
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    private void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 
-    
+
+
 }
