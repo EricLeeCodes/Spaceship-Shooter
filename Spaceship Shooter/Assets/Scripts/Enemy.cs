91f1e76cb9a6e2c8c0ac5e70734c2b2137643e3a
Eric Lee <87954849+EricLeeCodes@users.noreply.github.com>
2022-07-13 13:31:54 -0500

    Added enemy waves
    Added enemy waves in animation/timeline.

--- Spaceship Shooter/Assets/Scripts/Enemy.cs
+++ Spaceship Shooter/Assets/Scripts/Enemy.cs
@@ -7,18 +7,17 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] GameObject deathVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
-    [SerializeField] int enemyHitPoints = 4;
+    [SerializeField] int enemyHitPoints = 2;
 
     ScoreBoard scoreBoard;
-
+    GameObject parentGameObject;
     private void Start()
     {
         //FindObjectOfType is ONLY resource intensive IF put into ///UPDATE///
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
         AddRigidBody();
-
     }
 
     private void AddRigidBody()
@@ -39,7 +38,7 @@ public class Enemy : MonoBehaviour
     private void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         enemyHitPoints--;
         scoreBoard.IncreaseScore(scorePerHit);
     }
@@ -49,10 +48,13 @@ public class Enemy : MonoBehaviour
     private void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
 
+    void asd()
+    {
 
+    }
 
 }
